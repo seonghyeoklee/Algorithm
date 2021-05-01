7d11107cd551507a3ac5d8b3aceb2f3294fad193
seonghyeoklee <dltjdgur327@gmail.com>
2021-05-01 13:55:12 +0900

    1-7 풀이변경

--- 1-7 에라토스테네스 체/1-7.py
+++ 1-7 에라토스테네스 체/1-7.py
@@ -3,14 +3,14 @@ sys.stdin = open("1-7 에라토스테네스 체/input.txt")
 
 n = int(input())
 
-result = []
+ch = [0] * (n + 1)
+
+cnt = 0
 
 for i in range(2, n + 1):
-    cnt = 0
-    for j in range(2, i):
-        if (i % j) == 0:
-            cnt += 1
-    if cnt == 0:
-        result.append(i)
+    if ch[i] == 0:
+        cnt += 1
+        for j in range(i, n + 1, i):
+            ch[j] = 1
 
-print(len(result))
+print(cnt)
