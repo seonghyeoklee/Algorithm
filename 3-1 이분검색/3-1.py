a8ea4edca480dad0cd86f5fabb8fd251a5371015
seonghyeoklee <dltjdgur327@gmail.com>
2021-06-27 23:17:11 +0900

    Add 3-1 이분검색

---
+++ 3-1 이분검색/3-1.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open("3-1 이분검색/input.txt", "rt")
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+a.sort()
+lt = 0
+rt = n-1
+
+while lt <= rt:
+    mid = (lt + rt) // 2
+
+    if a[mid] == m:
+        print(mid+1)
+        break
+    elif a[mid] > m:
+        rt = mid - 1
+    else:
+        lt = mid + 1
