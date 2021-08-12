7d049f65912aaefbcf9831eaa817a019b8346e2b
seonghyeoklee <dltjdgur327@gmail.com>
2021-08-13 00:32:16 +0900

    선택정렬

---
+++ java/src/inflearn/sort/Main0601.java
@@ -0,0 +1,36 @@
+package inflearn.sort;
+
+import java.util.Scanner;
+
+class Main0601 {
+    public static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
